280939ff0ba456701eb1bb9636b1d99c6d705ce7
Samra01734 <Samramuqadasvu1@gmail.com>
2025-10-19 15:18:42 +0500

    data types

---
+++ Datatypes.js
@@ -0,0 +1,24 @@
+//lec 2
+//documentation cdn and ecma(define standards)
+alert("Hello World");
+//Data typ
+//premitive data type   ,call by value
+
+let name="Samra"//string  used in double and single quotes
+let age=23 //number
+let isLoggedIn=false //boolean
+
+//number =>2 to the power 53 
+//bigint  don't mostly used
+//null =>empty value ,standalone value
+//undefined =>  mean veriable def ho giya but value ni di
+//symbol => unique and immutable value
+
+
+//objects
+
+console.log(typeof "samra");//string
+//but null ki type find krin gye to yah 1 object show ho ga but undefine ki type undefined ha,this is language error
+//non-premitive or reference data type
+
+//arreys 2,objects,functions
