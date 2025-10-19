aa4a205a27945e2687f3fe9dc7140c92827804cd
Samra01734 <Samramuqadasvu1@gmail.com>
2025-10-19 15:32:57 +0500

    new feature

---
+++ string-num.js
@@ -0,0 +1,35 @@
+/**********lec 4*********/ 
+//oprations
+
+let value=3
+let negativeValue=-value
+console.log(negativeValue);
+
+//addition ,multiplication,subtraction,division
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);//multiplication
+console.log(2**3);//power
+console.log(2/3);//divide
+console.log(2%3);//remainder
+
+
+//concatination
+let str1="hello"
+let str2=" samra"
+let str3=str1+str2
+console.log(str3);
+
+
+console.log("10"+2+3);//1023
+console.log(2+3+"1");//51
+console.log("1"+2);//12
+
+
+//special conversion,increment and decrement
+console.log(+true)//but not accepted(true+)
+
+
+let gameCounter=100;
+gameCounter++;//increment
+console.log(gameCounter);//101,prefix and post fix
