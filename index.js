f01874f4deb3f44d367ba409e26471a7856c43ea
Samra01734 <Samramuqadasvu1@gmail.com>
2025-10-19 15:16:15 +0500

    let const var

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 
+// let const var ki khanai
+"use strict"; // use stric mood for new js code
+//  lec 1 variables etc
+const accountId=1234;
+let accountEmail="Samra@gmail.com";
+var accountPassword="1234";
+ let accountCity="Garhmore";
+
+
+console.table([accountId ,accountEmail,accountPassword,accountCity]);
